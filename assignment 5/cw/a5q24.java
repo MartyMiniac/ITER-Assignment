d6c2af89807dc493851f8a4a93c9afe90e349a4a
Rohan Verma <rohanvermaara@gmail.com>
2019-11-09 11:55:17 +0530

    a5q24.java working now

--- assignment 5/cw/a5q24.java
+++ assignment 5/cw/a5q24.java
@@ -5,13 +5,19 @@ class a5q24
 		int n=Integer.parseInt(args[0]);
 		for(int a=0;a<=n; a++)
 		{
-			int b=0;
-			while(true)
+			for(int b=0; b<=a; b++)
 			{
-				int c=0;
-				while(true)
+				for(int c=0; c<=(a*a*a+b*b*b);c++)
 				{
-					int d=0;
-					while(true)
+					for(int d=0; d<=(a*a*a+b*b*b); d++)
 					{
-						if((a*a*a+b*b*b)==(
+						if((a*a*a+b*b*b)==(c*c*c*+d*d*d))
+						{
+							System.out.println("a= "+a+"  b= "+b+"  c= "+c+"  d= "+d);
+						}
+					}
+				}
+			}
+		}
+	}
+}
