7157dd79cfb19eaf69a3b1e69a370ff0a5e31a3a
Rohan Verma <rohanvermaara@gmail.com>
2019-11-23 13:07:22 +0530

    a7q8.java question done (question updated)

--- assignment 7/a7q8.java
+++ assignment 7/a7q8.java
@@ -6,42 +6,40 @@ class a7q8
 		int arr[];
 		boolean k=true;
 		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the Value of n :");
+		int n=in.nextInt();
+		arr = new int[n];
+		for(int a=0; a<n; a++)
+		{
+			arr[a]=0;
+		}
 		while(k)
 		{
 			System.out.println("Choose Your Option :");
-			System.out.println("a. Create an array of n integers\nb. Display the array elements\nc. Insert an element at specific position\nd. Delete an element at a given position\ne. Exit");
+			System.out.println("a. Display the array elements\nb. Insert an element at specific position\nc. Delete an element at a given position\nd. Exit");
 			char c=in.next().charAt(0);
 			switch(c)
 			{
 				case 'a':
-					System.out.print("Enter the Value of n :");
-					int n=in.nextInt();
-					arr = new int[n];
-					for(int a=0; a<n; a++)
-					{
-						arr[a]=0;
-					}
-					break;
-				case 'b':
 					for(int a=0; a<arr.length; a++)
 					{
 						System.out.println(arr[a]);
 					}
 					break;
-				case 'c':
+				case 'b':
 					System.out.print("Enter the Value to Insert :");
 					int i=in.nextInt();
-					System.out.print("Enter the position number to Insert");
+					System.out.print("Enter the position number to Insert :");
 					int p=in.nextInt();
 					arr[p]=i;
 					break;
-				case 'd':
-					System.out.print("Enter the Position Number to Delete");
+				case 'c':
+					System.out.print("Enter the Position Number to Delete :");
 					int ps=in.nextInt();
-					System.out.print(arr[p]+" at position number "+p+" deleted");
+					System.out.println(arr[ps]+" at position number "+ps+" deleted");
 					arr[ps]=0;
 					break;
-				case 'e':
+				case 'd':
 					k=false;
 					break;
 				default :
