808de17d79b840435b98f01fe86dc39b05e55d0e
Rohan Verma <rohanvermaara@gmail.com>
2020-11-14 16:47:58 +0530

    Add and Update prog6.java

--- Semester 3 (CWS-1)/Assignments/Assignment on Interacting with the Environment/prog6.java
+++ Semester 3 (CWS-1)/Assignments/Assignment on Interacting with the Environment/prog6.java
@@ -1,10 +1,14 @@
 /*. Write a program which ask the user to enter a jar file name and check
 whether the jar file is install or not.*/
 import java.io.File;
+import java.util.*;
 
 public class prog6 {
     public static void main(String[] args) {
-        File f1 = new File("C:\\Users\\mayan\\Downloads");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the File Location : ");
+        File f1 = new File(in.nextLine());
+        in.close();
         String[] List = f1.list();
         for(String str1 : List) {
             int i = str1.lastIndexOf('.');
